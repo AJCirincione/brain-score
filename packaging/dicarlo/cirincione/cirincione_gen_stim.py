25971d6aba9d7e1038afa30139dcbf9fb9f37726
AJCirincione <acirincione01@manhattan.edu>
2022-07-04 13:55:11 -0400

    creating and packaging stimulus

--- packaging/dicarlo/cirincione/cirincione_gen_stim.py
+++ packaging/dicarlo/cirincione/cirincione_gen_stim.py
@@ -3,7 +3,9 @@ import numpy as np
 from cirincione_stim_common import gen_BO_stim, load_stim_info
 from brainio.packaging import package_stimulus_set
 
+
 BO_STIM_NAME = 'dicarlo.Cirincione2022_border_ownership'
+BO_OPTIM_NAME = 'dicarlo.Cirincione2022_bo_optim'
 DATA_DIR = '/braintree/data2/active/users/andrewci/stimulus_sets/' #(braintree stimulus folder)
 DEGREES = 12
 SIZE_PX = 672
@@ -12,21 +14,19 @@ POS_X = 0.5
 POS_Y = 0.5
 SQUARE_SIZE = 4 #degrees
 
-
 BO_PARAMS = np.array([DEGREES, SIZE_PX, ORIENTATION_DIV, POS_X, POS_Y, SQUARE_SIZE])
 
-STIM_NAMES = [BO_STIM_NAME]
-STIM_PARAMS = {BO_STIM_NAME: BO_PARAMS}
+STIM_NAMES = [BO_STIM_NAME, BO_OPTIM_NAME]
 
 generate_stimulus=1
 def main():
     for stim_name in STIM_NAMES:
-        stim_dir = DATA_DIR + stim_name
+        stim_dir = os.path.join(DATA_DIR, stim_name)
         if generate_stimulus==1:
             print('Generating...')
-            gen_BO_stim(BO_params = STIM_PARAMS[stim_name], save_dir = stim_dir)
+            gen_BO_stim(BO_params = BO_PARAMS, save_dir = stim_dir, stim_name=stim_name)
         stimuli = load_stim_info(stim_name, stim_dir)
-        print('Packaging stimuli:' + stimuli.identifier)
+        #print('Packaging stimuli:' + stimuli.identifier)
         #package_stimulus_set(stimuli, stimulus_set_identifier=stimuli.identifier, bucket_name='brainio.dicarlo')
 
 
--- packaging/dicarlo/cirincione/cirincione_stim_common.py
+++ packaging/dicarlo/cirincione/cirincione_stim_common.py
@@ -42,6 +42,55 @@ blank = {'red': None,
                   'gray': None,
                   'black': None,
                   'light_gray': None}
+class BO_Optim:
+    def __init__(self, save_dir, stim_size = 672, visual_degrees = 12,
+                                 divisions = 12, posy = 0.5, posx = 0.5):
+        self.save_dir = save_dir
+        self.stim_size = int(stim_size)
+        self.visual_degrees = visual_degrees
+        divisions = int(divisions)
+        self.divisions = int(divisions)
+        self.posx = int(posx)
+        self.posy = int(posy)
+        self.colors = xyY_to_RGB(xyY_COLOR_DICT, blank)
+        self.ground = self.colors['light_gray']
+        # pos array 0.5 0.5
+        width = [0.1, 0.2]
+        length = [0.75, 1.5]
+        ground_color = np.array([118.180, 124.506, 129.323])
+        BO_optim_stim_data = pd.DataFrame(
+            columns=['image_id', 'degrees', 'posy', 'posx', 'color', 'orientation', 'width',
+                     'length'])  # posx and posy offset
+        # image_id degrees posy posx color orientation width length
+        color_idx = 0
+        print('Constructing...')
+        for color_name in tqdm(list(self.colors.keys())[0:14]):
+            width_idx = 0
+            for W in width:
+                length_idx = 0
+                for L in length:
+                    BO_optim_stim_oris = generate_bar_stim(length=L / visual_degrees, width=W / visual_degrees,
+                                                           stim_size=stim_size,
+                                                           divisions=divisions, figure_color=self.colors[color_name],
+                                                           ground_color=ground_color,
+                                                           posx=posx / visual_degrees,
+                                                           posy=posy / visual_degrees)
+                    division_idx = 0
+                    for d in range(divisions):
+                        ID = str(color_idx).zfill(2) + str(width_idx).zfill(2) + str(length_idx).zfill(2) + str(
+                            division_idx).zfill(2)
+                        BO_optim_stim_img = BO_optim_stim_oris[d].astype(np.uint8)
+                        BO_optim_stim_data = BO_optim_stim_data.append(
+                            {'image_id': ID, 'degrees': visual_degrees, 'posy': posy, 'posx': posx, 'color': color_name,
+                             'orientation': 180 / 12 * d, 'width': W, 'length': L, },
+                            ignore_index=True)
+                        file_name = 'BO_optim_stim_' + str(ID) + '.png'
+                        imageio.imwrite(self.save_dir + file_name, BO_optim_stim_img)
+                        division_idx += 1
+                    length_idx += 1
+                width_idx += 1
+            color_idx += 1
+        BO_optim_stim_data.to_csv(self.save_dir + 'BO_optim_stim_data', index=False)
 
 class BO_Stimulus:
     def __init__(self, save_dir, stim_size = 672, visual_degrees = 12,
@@ -56,7 +105,7 @@ class BO_Stimulus:
         self.posy = int(posy)
         self.sqr_deg = int(sqr_deg)
         self.xyY_color_dict = xyY_COLOR_DICT
-        self.colors = self.xyY_to_RGB()
+        self.colors = xyY_to_RGB(xyY_COLOR_DICT, blank)
         self.ground = self.colors['light_gray']
 
         BO_standard_test_stim_data = pd.DataFrame(
@@ -79,7 +128,7 @@ class BO_Stimulus:
                         xshift = 0.5
                     else:
                         xshift = -0.5
-                    BO_standard_test_stim_oris = self.generate_bar_stim(length=self.sqr_deg / self.visual_degrees,
+                    BO_standard_test_stim_oris = generate_bar_stim(length=self.sqr_deg / self.visual_degrees,
                                                                    width=self.sqr_deg / self.visual_degrees,
                                                                    stim_size=self.stim_size,
                                                                    divisions=self.divisions, figure_color=figure,
@@ -105,44 +154,10 @@ class BO_Stimulus:
         BO_standard_test_stim_data.to_csv(DIR + 'stimulus_set', index=False)
         self.stim_data = BO_standard_test_stim_data
 
-    def generate_bar_stim(self, length, width, stim_size, divisions, figure_color, ground_color, xshift, yshift, posx, posy):
-        angles = np.linspace(90, -90, divisions, endpoint=False)
-
-        xshift = int((self.stim_size * xshift) // 2)
-        yshift = int((self.stim_size * yshift) // 2)
-        radius_L = int((self.stim_size * length) // 2)
-        radius_W = int((self.stim_size * width) // 2)
-
-        square_stim = np.zeros((self.divisions, self.stim_size, self.stim_size, 3))
-        surround_stim = np.zeros((self.divisions, self.stim_size * 2, self.stim_size * 2, 3))
-        surround_stim[:, :, :] = ground_color
-
-        origin = stim_size // 2
-        x_lim = np.linspace(2 * origin - radius_W, 2 * origin + radius_W, radius_W * 2, endpoint=False) + xshift
-        y_lim = np.linspace(2 * origin - radius_L, 2 * origin + radius_L, radius_L * 2, endpoint=False) + yshift
-
-        idx = 0
-        for ang in (angles):
-            for x in x_lim:
-                for y in y_lim:
-                    col = figure_color
-                    try:
-                        surround_stim[idx, int(np.floor(y)), int(np.floor(x))] = col
-                    except:
-                        pass
-            surround_rotated = nd.rotate(surround_stim[idx], ang, reshape=False, prefilter=False)
-            square_stim[idx] = surround_rotated[int((stim_size - 0.5 * stim_size) - posy * stim_size):int(
-                (stim_size + 0.5 * stim_size) - posy * stim_size),
-                               int((stim_size - 0.5 * stim_size) - posx * stim_size):int(
-                                   (stim_size + 0.5 * stim_size) - posx * stim_size)]
-            idx += 1
-        return square_stim.astype(int)
-
-
-    def xyY_to_RGB(self):
+def xyY_to_RGB(xyY_color_dict, blank):
         #Credit to https://www.easyrgb.com/en/math.php
-        xyY_color_dict = self.xyY_color_dict
-        rgb_color_dict = self.blank  # posx and posy offset
+        xyY_color_dict = xyY_color_dict
+        rgb_color_dict = blank  # posx and posy offset
         for keys in xyY_color_dict.keys():
             Y = xyY_color_dict[keys][2]
             X = xyY_color_dict[keys][0] * (Y / xyY_color_dict[keys][1])
@@ -176,6 +191,43 @@ class BO_Stimulus:
             rgb_color_dict[keys] = np.array([sR, sG, sB])
         return rgb_color_dict
 
+
+def generate_bar_stim(length, width, stim_size, divisions, figure_color, ground_color, xshift, yshift, posx, posy):
+
+        angles = np.linspace(90, -90, divisions, endpoint=False)
+
+        xshift = int((stim_size * xshift) // 2)
+        yshift = int((stim_size * yshift) // 2)
+        radius_L = int((stim_size * length) // 2)
+        radius_W = int((stim_size * width) // 2)
+
+        square_stim = np.zeros((divisions, stim_size, stim_size, 3))
+        surround_stim = np.zeros((divisions, stim_size * 2, stim_size * 2, 3))
+        surround_stim[:, :, :] = ground_color
+
+        origin = stim_size // 2
+        x_lim = np.linspace(2 * origin - radius_W, 2 * origin + radius_W, radius_W * 2, endpoint=False) + xshift
+        y_lim = np.linspace(2 * origin - radius_L, 2 * origin + radius_L, radius_L * 2, endpoint=False) + yshift
+
+        idx = 0
+        for ang in (angles):
+            for x in x_lim:
+                for y in y_lim:
+                    col = figure_color
+                    try:
+                        surround_stim[idx, int(np.floor(y)), int(np.floor(x))] = col
+                    except:
+                        pass
+            surround_rotated = nd.rotate(surround_stim[idx], ang, reshape=False, prefilter=False)
+            square_stim[idx] = surround_rotated[int((stim_size - 0.5 * stim_size) - posy * stim_size):int(
+                (stim_size + 0.5 * stim_size) - posy * stim_size),
+                               int((stim_size - 0.5 * stim_size) - posx * stim_size):int(
+                                   (stim_size + 0.5 * stim_size) - posx * stim_size)]
+            idx += 1
+        return square_stim.astype(int)
+
+
+
 def load_stim_info(stim_name, data_dir):
     stim = pd.read_csv(os.path.join(data_dir, 'stimulus_set'), dtype={'image_id': str})
     image_paths = dict((key, value) for (key, value) in zip(stim['image_id'].values,
@@ -187,18 +239,27 @@ def load_stim_info(stim_name, data_dir):
 
     return stim_set
 
-def gen_BO_stim(BO_params, save_dir):
+def gen_BO_stim(BO_params, save_dir, stim_name):
     if not (os.path.isdir(save_dir)):
         os.mkdir(save_dir)
    # BO_PARAMS = np.array([DEGREES, SIZE_PX, ORIENTATION_DIV,
     # POS_X, POS_Y, SQUARE_SIZE])
-
-    BO_stim = BO_Stimulus(save_dir=save_dir,
+    if stim_name == 'dicarlo.Cirincione2022_border_ownership':
+        BO_stim = BO_Stimulus(save_dir=save_dir,
                           visual_degrees=BO_params[0],
                           stim_size=BO_params[1],
                           divisions=BO_params[2],
                           posx=BO_params[3],
                           posy=BO_params[4],
                           sqr_deg=BO_params[5])
+
+    if stim_name == 'dicarlo.Cirincione2022_bo_optim':
+        BO_optim = BO_Optim(save_dir=save_dir,
+                        visual_degrees=BO_params[0],
+                        stim_size=BO_params[1],
+                        divisions=BO_params[2],
+                        posx=BO_params[3],
+                        posy=BO_params[4])
+
 
     return
