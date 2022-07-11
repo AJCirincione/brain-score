4dc93a675011a95829de271030f57c90ddf6fa2d
Andrew Cirincione <andrewci@braintree-cpu-1.mit.edu>
2022-07-11 14:13:56 -0400

    test

--- packaging/dicarlo/cirincione/cirincione2022_zhou2000_TESTING.py
+++ packaging/dicarlo/cirincione/cirincione2022_zhou2000_TESTING.py
@@ -1,5 +1,6 @@
 import numpy as np
 import sys
+from brainscore.model_interface import BrainModel
 sys.path.insert(1, '/braintree/home/andrewci/brain-score/')
 from candidate_models.model_commitments import brain_translated_pool
 #sys.path.insert(1, '/braintree/home/andrewci/brain-score/brainscore/benchmarks')
@@ -9,7 +10,7 @@ from brainscore.benchmarks._properties_common import get_stimulus_position, \
     filter_receptive_fields, firing_rates_affine, record_from_model, get_firing_rates, get_stimulus_position, filter_receptive_fields
 import matplotlib.pyplot as plt
 
-def modified_record_from_model(model, stimuli, number_of_trials):
+def modified_record_from_model(model:BrainModel, stimuli, number_of_trials):
     stimulus_set = stimuli
     stimulus_set = place_on_screen(stimulus_set, target_visual_degrees=model.visual_degrees())
     activations = model.look_at(stimulus_set, number_of_trials)
@@ -33,7 +34,6 @@ def modified_get_firing_rates(model_identifier, model, region, number_of_trials,
 
 def load_model_and_BO_stimulus(BO_OPTIM_NAME, optim_test_dir, BO_STIM_NAME, standard_test_dir, model_identifier='alexnet',
                      region= 'V1', time_bins = [(70, 170)], number_of_trials = 20):
-    model_identifier = 'alexnet'
     model = brain_translated_pool[model_identifier]
     model.start_recording(region, time_bins=time_bins)
 
@@ -51,7 +51,7 @@ def load_model_and_BO_stimulus(BO_OPTIM_NAME, optim_test_dir, BO_STIM_NAME, stan
                                         stimuli=standard_test_stimuli, number_of_trials=number_of_trials, in_rf=standard_stim_in_rf)
     return model, optimization_test_responses, standard_test_responses                                
 
-def BO_optimization_and_standard_test(neuroid_no, optimization_test_responses, standard_test_responses):
+def BO_optimization_test(neuroid_no, optimization_test_responses):
     opt_response = optimization_test_responses.values
     opt_n_neuroids = opt_response.shape[0]
     opt_color = np.array(sorted(set(optimization_test_responses.color.values)))
@@ -59,11 +59,24 @@ def BO_optimization_and_standard_test(neuroid_no, optimization_test_responses, s
     opt_width = np.array(sorted(set(optimization_test_responses.width.values)))
     opt_length = np.array(sorted(set(optimization_test_responses.length.values)))
     opt_response = opt_response.reshape((opt_n_neuroids, len(opt_color),
-            len(opt_width), len(opt_length),  len(opt_orientation)))
-    print(opt_response.shape)
-    print(opt_response)        
-   # opt_pref_color, opt_pref_width, opt_pref_length, opt_pref_orientation =\
-      #  np.unravel_index(np.argmax(opt_response[neuroid_no, :, :, :, :]), 
-       # (len(opt_color),  len(opt_width), len(opt_length), len(opt_orientation)))
+            len(opt_width), len(opt_length),  len(opt_orientation)))        
+    opt_pref_color, opt_pref_width, opt_pref_length, opt_pref_orientation =\
+        np.unravel_index(np.argmax(opt_response[neuroid_no, :, :, :, :]), 
+        (len(opt_color),  len(opt_width), len(opt_length), len(opt_orientation)))
+    return opt_pref_color, opt_pref_width, opt_pref_length, opt_pref_orientation
 
-
+def BO_standard_test(neuroid_no, standard_test_responses, opt_pref_color, opt_pref_orientation):
+    std_response = standard_test_responses.values
+    std_n_neuroids = std_response.shape[0]
+    std_color = np.array(sorted(set(standard_test_responses.color.values)))
+    std_orientation = np.array(sorted(set(standard_test_responses.orientation.values)))
+    std_polarity = np.array(sorted(set(standard_test_responses.polarity.values)))
+    std_side = np.array(sorted(set(standard_test_responses.side.values)))
+    std_response = std_response.reshape((std_n_neuroids, len(std_color),
+            len(std_polarity), len(std_side),  len(std_orientation)))     
+    std_A = std_response[neuroid_no, opt_pref_color, 0, 0, opt_pref_orientation]
+    std_B = std_response[neuroid_no, opt_pref_color, 1, 1, opt_pref_orientation]
+    std_C = std_response[neuroid_no, opt_pref_color, 1, 0, opt_pref_orientation]
+    std_D = std_response[neuroid_no, opt_pref_color, 0, 1, opt_pref_orientation]
+    print(std_response[neuroid_no, opt_pref_color, :, :, opt_pref_orientation])
+    return std_A, std_B, std_C, std_D
